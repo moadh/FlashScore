999d4ee0674812f202a7b5d4c8bba3c66c2dbde5
Nejibb <nejib.briki@m2b-group.com>
2015-02-19 16:16:02 -1100

    impl

---
+++ FlashScore.CORE/Services/LeagueService.cs
@@ -0,0 +1,32 @@
+﻿using FlashScore.CORE.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlashScore.CORE.Services
+{
+    public class LeagueService : ILeague
+    {
+        private IRepository _Repository;
+
+        public LeagueService(IRepository repository)
+        {
+            _Repository = repository;
+        }
+        public IList<Entity.Match> GetMatches(string competitionName, int year, int roundNumber)
+        {
+            //IList<Entity.Match> results;
+
+            //var competition = _Repository.GetCompetitions().Where(c => c.Name == competitionName && c.Year == year).FirstOrDefault();
+            //var round = _Repository.GetRounds().Where(r => r.Id == roundId);
+            return _Repository.GetMatchsByRound( competitionName,  year ,roundNumber);
+        }
+
+        public Entity.Competition GetMatches(string competitionName, int year)
+        {
+            return _Repository.GetMatchsByCompetition(competitionName, year);
+        }
+    }
+}
