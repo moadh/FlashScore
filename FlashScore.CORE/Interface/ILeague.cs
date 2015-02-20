e0dccc6ce7fa02d8b4c4025ae621e1a42fcdd7ee
Nejibb <nejib.briki@m2b-group.com>
2015-02-19 16:15:26 -1100

    add missin interface

---
+++ FlashScore.CORE/Interface/ILeague.cs
@@ -0,0 +1,15 @@
+﻿using FlashScore.CORE.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlashScore.CORE.Interface
+{
+    public interface ILeague
+    {
+        IList<Match> GetMatches(string competitionName, int year, int roundId);
+        Competition GetMatches(string competitionName, int year);
+    }
+}
